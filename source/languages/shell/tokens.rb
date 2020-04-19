c7d75680dd971ef0e2f0ac5bb53e714ebd3b93cc
derkallevombau <kalle@Aspire2>
2020-04-19 17:24:05 +0200

    tokens.rb: Moved spaces after comma so RuboCop doesn't complain.

--- source/languages/shell/tokens.rb
+++ source/languages/shell/tokens.rb
@@ -8,61 +8,58 @@
 # ! { } case do done elif else esac fi for if in then until while
 # [[ ]] function select
 
-# Shell Variables 
+# Shell Variables
 # ENV HOME IFS LANG LC_ALL LC_COLLATE LC_CTYPE LC_MESSAGES LINENO NLSPATH PATH PPID PS1 PS2 PS4 PWD
 
 # utils
 # alias bg cd command false fc fg getopts hash jobs kill newgrp pwd read true umask unalias wait
 
-require_relative '../../textmate_tools.rb'
+require_relative('../../../directory')
+require(PathFor[:textmate_tools])
 
-tokens = [
-    { representation: '|' , areInvalidLiterals: true },
-    { representation: '&' , areInvalidLiterals: true },
-    { representation: ';' , areInvalidLiterals: true },
-    { representation: '<' , areInvalidLiterals: true },
-    { representation: '>' , areInvalidLiterals: true },
-    { representation: '(' , areInvalidLiterals: true },
-    { representation: ')' , areInvalidLiterals: true },
-    { representation: '$' , areInvalidLiterals: true },
-    { representation: '`' , areInvalidLiterals: true },
-    { representation: '\\', areInvalidLiterals: true },
-    { representation: '"' , areInvalidLiterals: true },
-    { representation: '\'', areInvalidLiterals: true },
-    { representation: "function" , areNonCommands: true, },
-    { representation: "export"   , areNonCommands: true, },
-    { representation: "select"   , areNonCommands: true, },
-    { representation: "case"     , areNonCommands: true, },
-    { representation: "do"       , areNonCommands: true, },
-    { representation: "done"     , areNonCommands: true, },
-    { representation: "elif"     , areNonCommands: true, },
-    { representation: "else"     , areNonCommands: true, },
-    { representation: "esac"     , areNonCommands: true, },
-    { representation: "fi"       , areNonCommands: true, },
-    { representation: "for"      , areNonCommands: true, },
-    { representation: "if"       , areNonCommands: true, },
-    { representation: "in"       , areNonCommands: true, },
-    { representation: "then"     , areNonCommands: true, },
-    { representation: "until"    , areNonCommands: true, },
-    { representation: "while"    , areNonCommands: true, },
-    { representation: "alias"    , },
-    { representation: "bg"       , },
-    { representation: "command"  , },
-    { representation: "false"    , },
-    { representation: "fc"       , },
-    { representation: "fg"       , },
-    { representation: "getopts"  , },
-    { representation: "hash"     , },
-    { representation: "jobs"     , },
-    { representation: "kill"     , },
-    { representation: "newgrp"   , },
-    { representation: "read"     , },
-    { representation: "true"     , },
-    { representation: "umask"    , },
-    { representation: "unalias"  , },
-    { representation: "wait"     , },
-]
-
-
-
-@tokens = TokenHelper.new tokens
+@tokens = TokenHelper.new([
+	{ representation: '|',        areInvalidLiterals: true },
+	{ representation: '&',        areInvalidLiterals: true },
+	{ representation: ';',        areInvalidLiterals: true },
+	{ representation: '<',        areInvalidLiterals: true },
+	{ representation: '>',        areInvalidLiterals: true },
+	{ representation: '(',        areInvalidLiterals: true },
+	{ representation: ')',        areInvalidLiterals: true },
+	{ representation: '$',        areInvalidLiterals: true },
+	{ representation: '`',        areInvalidLiterals: true },
+	{ representation: '\\',       areInvalidLiterals: true },
+	{ representation: '"',        areInvalidLiterals: true },
+	{ representation: '\'',       areInvalidLiterals: true },
+	{ representation: 'function', areNonCommands: true, },
+	{ representation: 'export',   areNonCommands: true, },
+	{ representation: 'select',   areNonCommands: true, },
+	{ representation: 'case',     areNonCommands: true, },
+	{ representation: 'do',       areNonCommands: true, },
+	{ representation: 'done',     areNonCommands: true, },
+	{ representation: 'elif',     areNonCommands: true, },
+	{ representation: 'else',     areNonCommands: true, },
+	{ representation: 'esac',     areNonCommands: true, },
+	{ representation: 'fi',       areNonCommands: true, },
+	{ representation: 'for',      areNonCommands: true, },
+	{ representation: 'if',       areNonCommands: true, },
+	{ representation: 'in',       areNonCommands: true, },
+	{ representation: 'then',     areNonCommands: true, },
+	{ representation: 'until',    areNonCommands: true, },
+	{ representation: 'while',    areNonCommands: true, },
+	{ representation: 'alias',    },
+	{ representation: 'bg',       },
+	{ representation: 'command',  },
+	{ representation: 'false',    },
+	{ representation: 'fc',       },
+	{ representation: 'fg',       },
+	{ representation: 'getopts',  },
+	{ representation: 'hash',     },
+	{ representation: 'jobs',     },
+	{ representation: 'kill',     },
+	{ representation: 'newgrp',   },
+	{ representation: 'read',     },
+	{ representation: 'true',     },
+	{ representation: 'umask',    },
+	{ representation: 'unalias',  },
+	{ representation: 'wait',     },
+])
