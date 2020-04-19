4f1460d0288e5c95699366fb2de7952645e484f9
derkallevombau <kalle@Aspire2>
2020-04-19 17:30:10 +0200

    checkers.rb: Removed 'isEmptyRegexp':
    1. Unnecessary, use !isNonEmptyRegexp instead.
    
    2. Name is inappropriate: Returns true if arg is not even a Regexp.

--- source/textmate_tools/checkers.rb
+++ source/textmate_tools/checkers.rb
@@ -1,5 +1,3 @@
-def isNonEmptyRegexp(arg) arg.is_a?(Regexp) && arg != //  end
-def isEmptyRegexp(arg)    !arg.is_a?(Regexp) || arg == // end
-
-def isNonEmptyHash(arg)   arg.is_a?(Hash) && arg.length.positive?   end
-def isNonEmptyString(arg) arg.is_a?(String) && arg.length.positive? end
+def isNonEmptyRegexp(arg) arg.is_a?(Regexp) && arg != //   end
+def isNonEmptyHash(arg)   arg.is_a?(Hash) && !arg.empty?   end
+def isNonEmptyString(arg) arg.is_a?(String) && !arg.empty? end
