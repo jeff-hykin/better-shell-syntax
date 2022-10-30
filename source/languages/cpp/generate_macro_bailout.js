f08cbe611b3b8b5f99c310ae11b332ffa11df5fd
Jeff Hykin <jeff.hykin@gmail.com>
2022-10-30 15:17:39 -0500

    purge old

--- source/languages/cpp/generate_macro_bailout.js
+++
@@ -1,9 +0,0 @@
-let duplicateForEmbedding = require("textmate-bailout")
-let {argv} = require("process")
-
-duplicateForEmbedding({
-    grammarFilePath: argv[2],
-    appendScope: "macro",
-    bailoutPattern: "(?<!\\\\)\n",
-    newFileLocation: argv[3]
-})
