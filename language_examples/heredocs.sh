81cbe7b22744741a483983e9db7afc772b13c526
Jeff Hykin <jeff.hykin@gmail.com>
2022-12-13 13:29:10 -0600

    -

--- language_examples/heredocs.sh
+++ language_examples/heredocs.sh
@@ -19,3 +19,21 @@ EOF
 		# My script here
 EOF
 fi
+
+
+echo "here is some $SYNTAX coloring"
+cat << EOF > myfile.txt
+multiple line
+output into
+a file
+EOF
+echo "here is some more $SYNTAX coloring"
+
+
+echo "here is some $SYNTAX coloring"
+	cat <<-EOF > myfile.txt
+	multiple line
+	output into
+	a file
+	EOF
+echo "here is some more $SYNTAX coloring"
