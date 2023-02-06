cd2a4d0d4af1a88d68a4a5a5ad31a28ac7f1bbd4
Jeff Hykin <jeff.hykin@gmail.com>
2023-02-06 07:53:41 -0600

    minor fix

--- language_examples/#034.sh
+++ language_examples/#034.sh
@@ -9,8 +9,8 @@ for post in "${posts[@]}"; do
 	"${command_args[@]}" "$post"
 done
 
+echo "End"
 "${command_args[@]}" "$post"
  "${command_args[@]}" "$post"
 ${command_args[@]} "$post"
 
-echo "End"
